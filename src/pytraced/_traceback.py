1ac28522c5c71270a028ec4e399b6ae4d55d8fb4
Eris Fletcher <fletcher.eris@gmail.com>
2023-07-19 02:52:24 -0600

    refacted to allow for better testing

--- src/pytraced/_traceback.py
+++ src/pytraced/_traceback.py
@@ -1,3 +1,62 @@
+# """
+# _traceback.py:
+
+# This file contains all of the functions used for interating with Python stack frames.
+
+# Functions:
+#     - `get_frame` - Get the currently executing frame at a specified depth.
+#     - `extract_stack` - Get a `StackSummary` object from a `FrameType` object.
+# """
+# from sys import exc_info
+# from traceback import StackSummary, walk_stack
+# from types import FrameType
+
+# # pylint: disable=broad-exception-caught, broad-exception-raised, protected-access, ungrouped-imports
+
+# try:
+#     from sys import _getframe
+
+#     def get_frame(depth: int = 0) -> FrameType:
+#         """
+#         Get the currently executing frame at a specified depth.
+
+#         Parameters:
+#             - `depth: int = 0` - Depth at which to return the frame.
+
+#         Returns: `FrameType` - Currently executing frame at the specified depth.
+#         """
+#         return _getframe(depth + 1)  # offset current frame
+
+# except ImportError:
+
+#     def get_frame(depth: int = 0) -> FrameType:
+#         """
+#         Fallback to get the currently executing frame if `sys._getframe` does not exist.
+
+#         Parameters:
+#             - `depth: int = 0` - Depth at which to return the frame.
+
+#         Returns: `FrameType` - Currently executing frame at the specified depth.
+#         """
+#         try:
+#             raise Exception
+#         except Exception:
+#             frame = exc_info()[2].tb_frame.f_back  # type: ignore
+#             for _ in range(depth):
+#                 frame = frame.f_back  # type: ignore
+#             return frame  # type: ignore
+
+
+# def extract_stack(frame: FrameType) -> StackSummary:
+#     """
+#     Get a `StackSummary` object from a `FrameType` object.
+
+#     Parameters:
+#         - `frame: FrameType` - Frame from which to extrack the stack.
+
+#     Returns: `StackSummary` - StackSummary extracted from the frame.
+#     """
+#     return StackSummary.extract(walk_stack(frame), limit=None)
 """
 _traceback.py:
 
@@ -7,44 +66,45 @@ Functions:
     - `get_frame` - Get the currently executing frame at a specified depth.
     - `extract_stack` - Get a `StackSummary` object from a `FrameType` object.
 """
-from sys import exc_info
+import sys
 from traceback import StackSummary, walk_stack
 from types import FrameType
 
 # pylint: disable=broad-exception-caught, broad-exception-raised, protected-access, ungrouped-imports
 
-try:
-    from sys import _getframe
 
-    def get_frame(depth: int = 0) -> FrameType:
-        """
-        Get the currently executing frame at a specified depth.
+def _get_frame(depth: int = 0) -> FrameType:
+    """
+    Get the currently executing frame at a specified depth.
 
-        Parameters:
-            - `depth: int = 0` - Depth at which to return the frame.
+    Parameters:
+        - `depth: int = 0` - Depth at which to return the frame.
 
-        Returns: `FrameType` - Currently executing frame at the specified depth.
-        """
-        return _getframe(depth + 1)  # offset current frame
+    Returns: `FrameType` - Currently executing frame at the specified depth.
+    """
+    return sys._getframe(depth + 1)  # offset current frame
 
-except ImportError:
 
-    def get_frame(depth: int = 0) -> FrameType:
-        """
-        Fallback to get the currently executing frame if `sys._getframe` does not exist.
+def _get_frame_fallback(depth: int = 0) -> FrameType:
+    """
+    Fallback to get the currently executing frame if `sys._getframe` does not exist.
 
-        Parameters:
-            - `depth: int = 0` - Depth at which to return the frame.
+    Parameters:
+        - `depth: int = 0` - Depth at which to return the frame.
 
-        Returns: `FrameType` - Currently executing frame at the specified depth.
-        """
-        try:
-            raise Exception
-        except Exception:
-            frame = exc_info()[2].tb_frame.f_back  # type: ignore
-            for _ in range(depth):
-                frame = frame.f_back  # type: ignore
-            return frame  # type: ignore
+    Returns: `FrameType` - Currently executing frame at the specified depth.
+    """
+
+    try:
+        raise Exception
+    except Exception:
+        frame = sys.exc_info()[2].tb_frame  # type: ignore
+        for _ in range(depth + 1):
+            frame = frame.f_back  # type: ignore
+        return frame  # type: ignore
+
+
+get_frame = _get_frame if hasattr(sys, "_getframe") else _get_frame_fallback
 
 
 def extract_stack(frame: FrameType) -> StackSummary:
